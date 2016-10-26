f532565a9567930955aa02030421f720406d5d1c
Tikam Rai <tikam_ra1i@yahoo.com>
2016-10-26 18:28:36 +0545

    Third Commit

---
+++ resources/views/admin/options.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tikam
+ * Date: 10/26/16
+ * Time: 11:07 AM
+ */
---
+++ resources/views/admin/search.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tikam
+ * Date: 10/26/16
+ * Time: 10:52 AM
+ */
